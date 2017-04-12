8a478833f0a5e68eb5f7ea5ad48b1c4a2c4919cd
Zach Donnelly <zach@solutioncraft.net>
2017-04-12 17:08:17 -0400

    - Add href validation methods and tests

--- test/spec/windowLocationSubject_spec.js
+++ test/spec/windowLocationSubject_spec.js
@@ -1,5 +1,4 @@
 import WindowLocationSubject from '../../lib/subjects/windowLocationSubject';
-
 describe('WindowLocationSubject.getHref()', () => {
     it('should get back the set href value', () => {
         const href = 'foo';
@@ -13,6 +12,14 @@ describe('WindowLocationSubject.getHref()', () => {
             const window = { location: { href: 'foo' } };
         }
         expect(s.getHref()).toBe(window.location.href);
+    });
+});
+describe('WindowLocationSubject.setHref()', () => {
+    it('should throw an error if the href transport method is not http or https', () => {
+        const s = new WindowLocationSubject();
+        expect(() => {
+            s.setHref('ftp://foo.bar');
+        }).toThrowError();
     });
 });
 describe('WindowLocationSubject.getLastPart()', () => {
@@ -56,12 +63,42 @@ describe('WindowLocationSubject.getLastPart()', () => {
         s.setHref(href);
         expect(s.getLastPart()).toBe(location);
     });
+    it('can operate on href with many back-slashes', () => {
+        const lastPart = 'foobarbaz';
+        const href = 'http://foo/bar/baz/foobar/foobaz/' + lastPart;
+        const s = new WindowLocationSubject();
+        s.setHref(href);
+        expect(s.getLastPart()).toBe(lastPart);
+    });
 });
-describe('WindowLocationSubject.setMessage()', () => {
-    it('should get back the same message that was set in setHref()', () => {
+describe('WindowLocationSubject.getMessage()', () => {
+    it('should get back the same message that was set in setHref() if setMessage() was not called', () => {
         const s = new WindowLocationSubject();
         const href = 'foo';
         s.setHref(href);
         expect(s.getMessage()).toBe(href);
     });
+});
+describe('WindowLocationSubject.hrefHasTransportMethod()', () => {
+    it('should return false if the href contains no colon', () => {
+        const s = new WindowLocationSubject();
+        expect(s.hrefHasTransportMethod('foo')).toBe(false);
+    });
+    it('should return false if no string given before color', () => {
+        const s = new WindowLocationSubject();
+        expect(s.hrefHasTransportMethod('://')).toBe(false);
+    });
+    it('should return true if http:// given as href argument', () => {
+        const s = new WindowLocationSubject();
+        expect(s.hrefHasTransportMethod('http://')).toBe(true);
+    });
+    it('should return false if anything other than http or https protocols given', () => {
+        const s = new WindowLocationSubject();
+        expect(s.hrefHasTransportMethod('ftp://')).toBe(false);
+        expect(s.hrefHasTransportMethod('ssh://')).toBe(false);
+    });
+    it('should not be case sensitive', () => {
+        const s = new WindowLocationSubject();
+        expect(s.hrefHasTransportMethod('HTTP://')).toBe(true);
+    })
 });
